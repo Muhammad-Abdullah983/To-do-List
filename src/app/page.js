a152de0bf3277c5f5398c94c0b6b5b0c2a67e1ac
Muammad Abdullah <hafizabdullaharshad5@gmail.com>
2025-11-28 14:41:01 +0500

    change bg color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Todo from "./Todo/Todo";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50 dark:bg-black font-sans">
+    <div className="min-h-screen bg-black dark:bg-black font-sans">
       <main className="py-12">
         <Todo />
       </main>
